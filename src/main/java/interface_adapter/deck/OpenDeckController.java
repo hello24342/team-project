883078db66fbe457de6a8f8863b1f139518f725f
Xiaotong Zhu <xiaotong.zhu@mail.utoronto.ca>
2025-11-24 15:45:51 -0500

    (uc11):almost done with the opendeck

--- src/main/java/interface_adapter/deck/OpenDeckController.java
+++ src/main/java/interface_adapter/deck/OpenDeckController.java
@@ -2,17 +2,16 @@ package interface_adapter.deck;
 
 import usecase.deck.open_deck.OpenDeckInputBoundary;
 import usecase.deck.open_deck.OpenDeckInputData;
-import usecase.deck.open_deck.OpenDeckOutputData;
 
 public class OpenDeckController {
-    private final OpenDeckInputBoundary openDeckInteractor;
 
-    public OpenDeckController(OpenDeckInputBoundary openDeckInteractor) {
-        this.openDeckInteractor = openDeckInteractor;
+    private final OpenDeckInputBoundary interactor;
+
+    public OpenDeckController(OpenDeckInputBoundary interactor) {
+        this.interactor = interactor;
     }
 
-    public void execute(String deckName) {
-        OpenDeckInputData inputData = new OpenDeckInputData(deckName);
-        openDeckInteractor.execute(inputData);
+    public void open(int deckId) {
+        interactor.open(new OpenDeckInputData(deckId));
     }
 }
