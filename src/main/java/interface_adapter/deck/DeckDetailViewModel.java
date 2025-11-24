fffb44ee5ada308b8504314e5458cf91a0249059
Xiaotong Zhu <xiaotong.zhu@mail.utoronto.ca>
2025-11-24 15:48:45 -0500

    (uc11) update DeckDetailView & DeckDetailViewModel & DeckManageUseCaseFactory

--- src/main/java/interface_adapter/deck/DeckDetailViewModel.java
+++ src/main/java/interface_adapter/deck/DeckDetailViewModel.java
@@ -1,27 +1,54 @@
 package interface_adapter.deck;
 
-import entity.FlashcardDeck;
-import entity.Flashcard;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
 import java.util.List;
-//TODO: this viewmodel is incomplete and just skeleton, add more fields as necessary
+
 public class DeckDetailViewModel {
 
-    private FlashcardDeck currentDeck;
-    private List<Flashcard> flashcards;
+    public static final String DETAIL_PROPERTY = "deckDetail";
+    public static final String ERROR_PROPERTY = "error";
 
-    public FlashcardDeck getCurrentDeck() {
-        return currentDeck;
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    private int deckId = -1;
+    private String deckTitle = "";
+    private List<CardVM> cards = new ArrayList<>();
+    private String errorMessage = null;
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        support.addPropertyChangeListener(l);
     }
 
-    public void setCurrentDeck(FlashcardDeck deck) {
-        this.currentDeck = deck;
+    public int getDeckId() { return deckId; }
+    public String getDeckTitle() { return deckTitle; }
+    public List<CardVM> getCards() { return new ArrayList<>(cards); }
+    public String getErrorMessage() { return errorMessage; }
+
+    public void setDetail(int deckId, String deckTitle, List<CardVM> cards) {
+        int oldId = this.deckId;
+        this.deckId = deckId;
+        this.deckTitle = deckTitle;
+        this.cards = new ArrayList<>(cards);
+        support.firePropertyChange(DETAIL_PROPERTY, oldId, deckId);
     }
 
-    public List<Flashcard> getFlashcards() {
-        return flashcards;
+    public void setError(String msg) {
+        String old = this.errorMessage;
+        this.errorMessage = msg;
+        support.firePropertyChange(ERROR_PROPERTY, old, msg);
     }
 
-    public void setFlashcards(List<Flashcard> flashcards) {
-        this.flashcards = flashcards;
+    public static class CardVM {
+        public final int id;
+        public final String sourceWord;
+        public final String targetWord;
+
+        public CardVM(int id, String sourceWord, String targetWord) {
+            this.id = id;
+            this.sourceWord = sourceWord;
+            this.targetWord = targetWord;
+        }
     }
 }
