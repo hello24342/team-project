e338f39212dfe1c1f7e23ce2880869b78490c110
Hannah <hannah.tan@mail.utoronto.ca>
2025-12-01 07:08:06 -0500

    edit flashcard

--- src/main/java/interface_adapter/edit_flashcard/EditFlashcardState.java
+++ src/main/java/interface_adapter/edit_flashcard/EditFlashcardState.java
@@ -1,4 +1,50 @@
 package interface_adapter.edit_flashcard;
 
 public class EditFlashcardState {
+
+    private int flashcardId;
+    private String sourceWord;
+    private String targetWord;
+    private String message;      // success
+    private String errorMessage; // error
+
+    public int getFlashcardId() {
+        return flashcardId;
+    }
+
+    public void setFlashcardId(int flashcardId) {
+        this.flashcardId = flashcardId;
+    }
+
+    public String getSourceWord() {
+        return sourceWord;
+    }
+
+    public void setSourceWord(String sourceWord) {
+        this.sourceWord = sourceWord;
+    }
+
+    public String getTargetWord() {
+        return targetWord;
+    }
+
+    public void setTargetWord(String targetWord) {
+        this.targetWord = targetWord;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
