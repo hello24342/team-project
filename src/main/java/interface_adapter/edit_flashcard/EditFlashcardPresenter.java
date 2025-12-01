806ca8bc16cb32bc04d822a00bae2584e01cee63
hello24342 <hellojennifer.du@mail.utoronto.ca>
2025-12-01 13:11:45 -0500

    Revert "hannah23"

--- src/main/java/interface_adapter/edit_flashcard/EditFlashcardPresenter.java
+++ src/main/java/interface_adapter/edit_flashcard/EditFlashcardPresenter.java
@@ -1,32 +1,4 @@
 package interface_adapter.edit_flashcard;
 
-import use_case.flashcard.edit.EditFlashcardOutputBoundary;
-import use_case.flashcard.edit.EditFlashcardOutputData;
-
-public class EditFlashcardPresenter implements EditFlashcardOutputBoundary {
-
-    private final EditFlashcardViewModel viewModel;
-
-    public EditFlashcardPresenter(EditFlashcardViewModel viewModel) {
-        this.viewModel = viewModel;
-    }
-
-    @Override
-    public void prepareSuccessView(EditFlashcardOutputData outputData) {
-        EditFlashcardState state = viewModel.getState();
-        state.setFlashcardId(outputData.getFlashcardId());
-        state.setMessage(outputData.getMessage());
-        state.setError(null);
-        viewModel.setState(state);
-        viewModel.firePropertyChange();
-    }
-
-    @Override
-    public void prepareFailView(String errorMessage) {
-        EditFlashcardState state = viewModel.getState();
-        state.setError(errorMessage);
-        state.setMessage(null);
-        viewModel.setState(state);
-        viewModel.firePropertyChange();
-    }
+public class EditFlashcardPresenter {
 }
--- src/main/java/interface_adapter/edit_flashcard/EditFlashcardViewModel.java
+++ src/main/java/interface_adapter/edit_flashcard/EditFlashcardViewModel.java
@@ -2,11 +2,8 @@ package interface_adapter.edit_flashcard;
 
 import interface_adapter.ViewModel;
 
-public class EditFlashcardViewModel extends ViewModel<EditFlashcardState> {
-
+public class EditFlashcardViewModel extends ViewModel {
     public EditFlashcardViewModel(String viewName) {
         super(viewName);
-        super("edit flashcard");
-        setState(new EditFlashcardState());
     }
 }
